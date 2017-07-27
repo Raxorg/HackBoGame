d640e9c742603ca4c02e8920d6e0233d723d5cfc
Joaco99 <viscafe38@gmail.com>
2017-07-27 18:14:41 -0400

    Assests

--- core/src/com/viscadevs/hackbo/HackBoGame.java
+++ core/src/com/viscadevs/hackbo/HackBoGame.java
@@ -1,18 +1,20 @@
 package com.viscadevs.hackbo;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.viscadevs.screens.GameScreen;
 
 public class HackBoGame extends Game {
-
-    public static final SpriteBatch batch = new SpriteBatch();
-
-    @Override
-    public void create() {
-
-    }
-
-    @Override
-    public void dispose() {
-    }
+	public static SpriteBatch batch;
+	public static BitmapFont font;
+	
+	@Override
+	public void create () {
+		setScreen(new GameScreen());
+	}
+	
+	@Override
+	public void dispose () {
+	}
 }
