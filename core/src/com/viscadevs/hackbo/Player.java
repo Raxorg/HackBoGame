9304d780ed591125286516ed7ab15d26754f73e1
Kevinsan94 <kevinviscafe@gmail.com>
2017-07-27 18:31:28 -0400

    Player

--- core/src/com/viscadevs/hackbo/Player.java
+++ core/src/com/viscadevs/hackbo/Player.java
@@ -1,5 +1,6 @@
 package com.viscadevs.hackbo;
 
+import com.badlogic.gdx.math.Vector2;
 import com.viscadevs.util.Constants;
 
 /**
@@ -8,20 +9,79 @@ import com.viscadevs.util.Constants;
 
 public class Player {
 
-    public String name;
-    public String gender;
-    public int money = 0;
-    public int health = Constants.INITIAL_HEALTH;
-    public int friends = 0;
-    public int happiness = -20;
+    private String name;
+    private String gender;
+    private int money = 0;
+    private int health = Constants.INITIAL_HEALTH;
+    private int friends = 0;
+    private int happiness = -20;
+    private Vector2 position;
 
-    public Player (String name, int money, String gender, int health, int friends, int happiness){
+
+    public Player (String name, int money, String gender, int health, int friends, int happiness, Vector2 position){
+
         this.name = name;
         this.gender = gender;
         this.money = money;
         this.health = health;
         this.friends = friends;
         this.happiness = happiness;
+        this.position = position;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getFriends() {
+        return friends;
+    }
+
+    public void setFriends(int friends) {
+        this.friends = friends;
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
 }
