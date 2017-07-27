1709c4bc03194a12086adc4f53e4691ed457f644
Luis Frontanilla <lrdolucho@gmail.com>
2017-07-27 18:32:43 -0400

    some progress

--- core/src/com/viscadevs/screens/Splash.java
+++ core/src/com/viscadevs/screens/Splash.java
@@ -1,7 +1,11 @@
 package com.viscadevs.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.viscadevs.util.Enums;
 
@@ -11,21 +15,32 @@ public class Splash extends ScreenAdapter {
     private Texture splashImage;
     private Enums.SplashState state;
     private float alpha = 0;
+    private ShapeRenderer renderer;
 
     @Override
     public void show() {
         state = Enums.SplashState.FADING_IN;
         startTime = TimeUtils.nanoTime();
+        renderer = new ShapeRenderer();
+        renderer.setAutoShapeType(true);
     }
 
     public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        renderer.setColor(0, 0, 0, alpha);
+        renderer.begin();
+        renderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        renderer.end();
         switch (state) {
             case FADING_IN:
-                alpha += 0.05f;
+                alpha = alpha + 0.05f > 1 ? 1 : alpha + 0.05f;
+                System.out.println(alpha);
                 break;
             case FADING_OUT:
-                alpha -= 0.05f;
+                alpha = alpha - 0.05f < 0 ? 0 : alpha - 0.05f;
+                System.out.println(alpha);
                 break;
             default:
                 break;
