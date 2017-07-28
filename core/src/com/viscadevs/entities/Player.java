b7f2a288839c6e4c3eea2b47d61499c11b07a52f
Joaco99 <viscafe38@gmail.com>
2017-07-28 11:31:47 -0400

    Assets

--- core/src/com/viscadevs/entities/Player.java
+++ core/src/com/viscadevs/entities/Player.java
@@ -3,6 +3,7 @@ package com.viscadevs.entities;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -15,6 +16,9 @@ public class Player {
 
     private String name;
 
+    private Texture standing;
+    private Animation<Texture> walkingRightAnimation, walkingLeftAnimation;
+
     private int money;
     private int health;
     private int popularity = 0;
@@ -24,7 +28,7 @@ public class Player {
     private Enums.Gender gender;
     private Enums.Facing facing = Enums.Facing.RIGHT;
     private Enums.WalkState walkState = Enums.WalkState.STANDING;
-    private Enums.State state;
+    private Enums.Level level;
 
     private Vector2 position;
 
@@ -37,9 +41,11 @@ public class Player {
         this.happiness = happiness;
         position = new Vector2(Constants.PLAYER_SPAWN_X, Constants.PLAYER_SPAWN_Y);
 
-        state = Enums.State.STANDARD;
+        level = Enums.Level.POOR;
         money = Constants.INITIAL_MONEY;
         health = Constants.INITIAL_HEALTH;
+
+        updateDrawing();
     }
 
     private void moveLeft(float delta) {
@@ -60,8 +66,49 @@ public class Player {
         position.x += delta * Constants.PLAYER_SPEED;
     }
 
+    public void updateDrawing() {
+
+        switch (level) {
+            case POOR:
+                if (gender == Enums.Gender.MALE) {
+                    standing = Assets.getInstance().playerAssets.malePoorFront;
+                    walkingRightAnimation = Assets.getInstance().playerAssets.malePoorWalkingRightAnimation;
+                    walkingLeftAnimation = Assets.getInstance().playerAssets.malePoorWalkingLeftAnimation;
+                }else{
+                    standing = Assets.getInstance().playerAssets.femalePoorFront;
+                    walkingRightAnimation = Assets.getInstance().playerAssets.femalePoorWalkingRightAnimation;
+                    walkingLeftAnimation = Assets.getInstance().playerAssets.femalePoorWalkingLeftAnimation;
+                }
+                break;
+            case MEDIUM:
+                if (gender == Enums.Gender.MALE) {
+                    standing = Assets.getInstance().playerAssets.maleMediumFront;
+                    walkingRightAnimation = Assets.getInstance().playerAssets.maleMediumWalkingRightAnimation;
+                    walkingLeftAnimation = Assets.getInstance().playerAssets.maleMediumWalkingLeftAnimation;
+                }else{
+                    standing = Assets.getInstance().playerAssets.femaleMediumFront;
+                    walkingRightAnimation = Assets.getInstance().playerAssets.femaleMediumWalkingRightAnimation;
+                    walkingLeftAnimation = Assets.getInstance().playerAssets.femaleMediumWalkingLeftAnimation;
+                }
+                break;
+            case RICH:
+                if (gender == Enums.Gender.MALE) {
+                    standing = Assets.getInstance().playerAssets.maleRichFront;
+                    walkingRightAnimation = Assets.getInstance().playerAssets.maleRichWalkingRightAnimation;
+                    walkingLeftAnimation = Assets.getInstance().playerAssets.maleRichWalkingLeftAnimation;
+                }else{
+                    standing = Assets.getInstance().playerAssets.femaleRichFront;
+                    walkingRightAnimation = Assets.getInstance().playerAssets.femaleRichWalkingRightAnimation;
+                    walkingLeftAnimation = Assets.getInstance().playerAssets.femaleRichWalkingLeftAnimation;
+                }
+                break;
+        }
+
+    }
+
 
     public void update(float delta) {
+        updateDrawing();
         boolean left = Gdx.input.isKeyPressed(Input.Keys.LEFT);
         boolean right = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
 
@@ -79,13 +126,13 @@ public class Player {
         Texture region = Assets.getInstance().playerAssets.maleMediumFront;
 
         if (walkState == Enums.WalkState.STANDING) {
-            region = Assets.getInstance().playerAssets.maleMediumFront;
+            region = standing;
         } else if (facing == Enums.Facing.RIGHT && walkState == Enums.WalkState.WALKING) {
             float walkTimeSeconds = ViscaUtils.secondsSince(walkStartTime);
-            region = Assets.getInstance().playerAssets.maleMediumWalkingRightAnimation.getKeyFrame(walkTimeSeconds);
+            region = walkingRightAnimation.getKeyFrame(walkTimeSeconds);
         } else if (facing == Enums.Facing.LEFT && walkState == Enums.WalkState.WALKING) {
             float walkTimeSeconds = ViscaUtils.secondsSince(walkStartTime);
-            region = Assets.getInstance().playerAssets.maleMediumWalkingLeftAnimation.getKeyFrame(walkTimeSeconds);
+            region = walkingLeftAnimation.getKeyFrame(walkTimeSeconds);
         }
 
         batch.draw(region, position.x, position.y, Constants.HUMAN_WIDTH, Constants.HUMAN_HEIGHT);
@@ -147,8 +194,8 @@ public class Player {
         this.position = position;
     }
 
-    public Enums.State getState() {
-        return state;
+    public Enums.Level getLevel() {
+        return level;
     }
 
     public int getEnergy() {
