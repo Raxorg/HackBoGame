21f7fa1744fd265829377d2765228a59dd5102e3
Joaco99 <viscafe38@gmail.com>
2017-07-28 02:27:08 -0400

    some changes

---
+++ core/src/com/viscadevs/util/Button.java
@@ -0,0 +1,27 @@
+package com.viscadevs.util;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Joaco99 on 28/07/2017.
+ */
+
+public class Button {
+    Texture texture;
+    Rectangle rectangle;
+
+    public Button(Texture texture, Vector2 position, Vector2 bounds){
+        this.texture = texture;
+        rectangle.set(position.x, position.y, bounds.x, bounds.y);
+    }
+
+    public Button(Texture texture, float x, float y, float width, float height){
+        this(texture, new Vector2(x, y), new Vector2(width, height));
+    }
+
+    public boolean isTouched(Vector2 positionTouched){
+        return rectangle.contains(positionTouched.x, positionTouched.y);
+    }
+}
