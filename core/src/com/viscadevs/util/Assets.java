99bb70dc4f51a479cedbda2fbb28e499b73a5a08
Joaco99 <viscafe38@gmail.com>
2017-07-27 21:41:36 -0400

    Player Animation

--- core/src/com/viscadevs/util/Assets.java
+++ core/src/com/viscadevs/util/Assets.java
@@ -18,10 +18,10 @@ public class Assets implements Disposable {
 
     public void init(AssetManager assetManager) {
         this.assetManager = assetManager;
+        assetManager.finishLoading();
 
         load();
 
-        playerAssets = new PlayerAssets();
     }
 
     private void load() {
@@ -40,23 +40,34 @@ public class Assets implements Disposable {
         public final Animation walkingRightAnimation, walkingLeftAnimation;
 
         public PlayerAssets() {
-            standingRight = assetManager.get(Constants.PERSON, Texture.class);
+            standingRight = assetManager.get(Constants.STANDING_RIGHT, Texture.class);
 
             Array<Texture> walkingRightFrames = new Array<Texture>();
-            walkingRightFrames.add(new Texture(Constants.WALKING_RIGHT_1));
-            walkingRightFrames.add(new Texture(Constants.WALKING_RIGHT_2));
-            walkingRightFrames.add(new Texture(Constants.WALKING_RIGHT_3));
+            walkingRightFrames.add(assetManager.get(Constants.WALKING_RIGHT_1, Texture.class));
+            walkingRightFrames.add(assetManager.get(Constants.WALKING_RIGHT_2, Texture.class));
+            walkingRightFrames.add(assetManager.get(Constants.WALKING_RIGHT_3, Texture.class));
 
             walkingRightAnimation = new Animation(Constants.WALK_LOOP_DURATION, walkingRightFrames, Animation.PlayMode.LOOP_PINGPONG);
 
-            standingLeft = new Texture(Constants.STANDING_LEFT);
+            standingLeft = assetManager.get(Constants.STANDING_LEFT, Texture.class);
 
             Array<Texture> walkingLeftFrames = new Array<Texture>();
-            walkingRightFrames.add(new Texture(Constants.WALKING_LEFT_1));
-            walkingRightFrames.add(new Texture(Constants.WALKING_LEFT_2));
-            walkingRightFrames.add(new Texture(Constants.WALKING_LEFT_3));
+            walkingRightFrames.add(assetManager.get(Constants.WALKING_LEFT_1, Texture.class));
+            walkingRightFrames.add(assetManager.get(Constants.WALKING_LEFT_2, Texture.class));
+            walkingRightFrames.add(assetManager.get(Constants.WALKING_LEFT_3, Texture.class));
 
             walkingLeftAnimation = new Animation(Constants.WALK_LOOP_DURATION, walkingLeftFrames, Animation.PlayMode.LOOP_PINGPONG);
+        }
+
+        public void load(){
+            assetManager.load(Constants.STANDING_LEFT, Texture.class);
+            assetManager.load(Constants.STANDING_RIGHT, Texture.class);
+            assetManager.load(Constants.WALKING_LEFT_1, Texture.class);
+            assetManager.load(Constants.WALKING_LEFT_2, Texture.class);
+            assetManager.load(Constants.WALKING_LEFT_3, Texture.class);
+            assetManager.load(Constants.WALKING_RIGHT_1, Texture.class);
+            assetManager.load(Constants.WALKING_RIGHT_2, Texture.class);
+            assetManager.load(Constants.WALKING_RIGHT_3, Texture.class);
         }
     }
 
