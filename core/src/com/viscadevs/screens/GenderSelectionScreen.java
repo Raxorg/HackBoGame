c9d8eb3f72e924d72636aa26f87de2c8421ab4fa
Joaco99 <viscafe38@gmail.com>
2017-07-28 13:31:24 -0400

    Gender

--- core/src/com/viscadevs/screens/GenderSelectionScreen.java
+++ core/src/com/viscadevs/screens/GenderSelectionScreen.java
@@ -1,9 +1,15 @@
 package com.viscadevs.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.viscadevs.hackbo.HackBoGame;
+import com.viscadevs.util.Assets;
 import com.viscadevs.util.Button;
 import com.viscadevs.util.Enums;
+
+import static com.viscadevs.hackbo.HackBoGame.batch;
 
 
 public class GenderSelectionScreen extends ScreenAdapter {
@@ -16,14 +22,36 @@ public class GenderSelectionScreen extends ScreenAdapter {
 
     @Override
     public void show() {
-        game.setScreen(new GameScreen(Enums.Gender.MALE));
+        maleButton = new Button(Assets.getInstance().buttonAssets.male,
+                Gdx.graphics.getWidth() / 7, Gdx.graphics.getHeight() / 3,
+                Gdx.graphics.getWidth() / 7, Gdx.graphics.getHeight() / 3, Color.WHITE) {
+            @Override
+            public void onTouch() {
+                game.setScreen(new GameScreen(Enums.Gender.MALE));
+            }
+        };
+        femaleButton = new Button(Assets.getInstance().buttonAssets.female,
+                Gdx.graphics.getWidth() * 5/ 7, Gdx.graphics.getHeight() / 3,
+                Gdx.graphics.getWidth()/ 7, Gdx.graphics.getHeight() / 3, Color.WHITE) {
+            @Override
+            public void onTouch() {
+                game.setScreen(new GameScreen(Enums.Gender.FEMALE));
+            }
+        };
     }
 
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(Color.SKY.r, Color.SKY.g, Color.SKY.b, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+
         maleButton.render();
         femaleButton.render();
+
+        batch.end();
     }
 
     @Override
